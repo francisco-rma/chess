e04a92faf03ec24309a8036c9cb5cdee938a0eb2
Francisco Andrade <frma059@gmail.com>
2025-09-11 19:47:02 -0300

    feat: basic form submission

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,41 @@
 import './App.css';
 
 const App = () => {
-  return (
+  const layout = (
     <div className="content">
       <h1>Hello, World</h1>
       <p>Start building amazing things with Rsbuild.</p>
+      <Form />
     </div>
   );
+  return layout;
 };
+
+function click(value) {
+  console.log('this was a click');
+  const myForm = value.view.document.getElementById('myForm');
+  console.log('this is my form:', myForm);
+  const formData = new FormData(myForm);
+  console.log('this is my form data:', formData);
+}
+
+function submit(event) {
+  event.preventDefault();
+  console.log('Event:', event);
+}
+
+function Form() {
+  return (
+    <form className='form' id='myForm' onSubmit={submit}>
+      <input id='sometext' name='sometext' type='text' placeholder='some text'></input>
+      <button aria-label='click' type='button' onClick={click}>
+        <span>Click</span>
+      </button>
+      <button aria-label='submit' type='submit'>
+        <span>Submit</span>
+      </button>
+    </form>
+  )
+}
 
 export default App;
