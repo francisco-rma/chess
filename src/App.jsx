8f57652940d7fbfd6f108a06aff37d8eefd9f06b
Francisco Andrade <frma059@gmail.com>
2025-09-11 19:54:54 -0300

    fix: removing global functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,29 @@
 import './App.css';
 
 const App = () => {
-  const layout = (
+  return (
     <div className="content">
       <h1>Hello, World</h1>
       <p>Start building amazing things with Rsbuild.</p>
       <Form />
     </div>
-  );
-  return layout;
+  )
 };
 
-function click(value) {
-  console.log('this was a click');
-  const myForm = value.view.document.getElementById('myForm');
-  console.log('this is my form:', myForm);
-  const formData = new FormData(myForm);
-  console.log('this is my form data:', formData);
-}
-
-function submit(event) {
-  event.preventDefault();
-  console.log('Event:', event);
-}
-
 function Form() {
+  const click = (event) => {
+    console.log('this was a click');
+    const myForm = event.view.document.getElementById('myForm');
+    console.log('this is my form:', myForm);
+    const formData = new FormData(myForm);
+    console.log('this is my form data:', formData);
+  }
+
+  const submit = (event) => {
+    event.preventDefault();
+    console.log('Event:', event);
+  }
+
   return (
     <form className='form' id='myForm' onSubmit={submit}>
       <input id='sometext' name='sometext' type='text' placeholder='some text'></input>
