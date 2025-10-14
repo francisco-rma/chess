5aa30d007d09c81717d9d280611497ab55f89151
Francisco Andrade <frma059@gmail.com>
2025-10-13 22:17:05 -0300

    encapsulating range validation logic and determining valid rook moves

--- src/ChessLogic.jsx
+++ src/ChessLogic.jsx
@@ -1,3 +1,43 @@
+function validateRange(source, target, board, rowStep, colStep) {
+    const sourcePiece = board[source.row][source.col]
+    
+    const rowShift = target.row - source.row
+    const colShift = target.col - source.col
+    
+    if (rowShift !== 0 && colShift !== 0) {
+        return result
+    }
+    
+    let result = [];
+    let i = 1
+
+    for (const value of iterate(board, source, rowStep, colStep, Math.max(Math.abs(rowShift), Math.abs(colShift)))) {
+        if (!value) {
+            result.push({ row: source.row + rowStep * i, col: source.col + colStep * i })
+        } else {
+            if (value.color !== sourcePiece.color)
+                result.push({ row: source.row + rowStep * i, col: source.col + colStep * i })
+            break
+        }
+        i += 1
+    }
+    return result
+}
+
+function* iterate(board, source, rowStep, colStep, count) {
+    let rowOffset = 0
+    let colOffset = 0
+
+    for (let i = 0; i < count; i++) {
+        rowOffset += rowStep
+        colOffset += colStep
+
+        const element = board[source.row + rowOffset][source.col + colOffset]
+        yield element
+    }
+    return
+}
+
 function isValidPawnMove(source, target, board, lastMove) {
     const sourcePiece = board[source.row][source.col]
     const targetPiece = board[target.row][target.col]
@@ -56,18 +96,15 @@ function isValidRookMove(source, target, board) {
     const rowStep = rowShift === 0 ? 0 : rowShift > 0 ? 1 : -1
     const colStep = colShift === 0 ? 0 : colShift > 0 ? 1 : -1
 
-    let currentRow = source.row + rowStep
-    let currentCol = source.col + colStep
+    let result = true
 
-    while (currentRow !== target.row || currentCol !== target.col) {
-        if (board[currentRow][currentCol]) {
-            return false // Path is blocked
+    for (const value of iterate(board, source, rowStep, colStep, Math.max(Math.abs(rowShift), Math.abs(colShift)))) {
+        if (value) {
+            result = false
         }
-        currentRow += rowStep
-        currentCol += colStep
     }
 
-    return true
+    return result
 }
 
 function isValidKnightMove(source, target) {
@@ -88,18 +125,16 @@ function isValidBishopMove(source, target, board) {
     const rowStep = rowShift > 0 ? 1 : -1
     const colStep = colShift > 0 ? 1 : -1
 
-    let currentRow = source.row + rowStep
-    let currentCol = source.col + colStep
+    let result = true
 
-    while (currentRow !== target.row || currentCol !== target.col) {
-        if (board[currentRow][currentCol]) {
-            return false // Path is blocked
+    for (const value of iterate(board, source, rowStep, colStep, Math.abs(rowShift))) {
+        if (value) {
+            result = false
+            break
         }
-        currentRow += rowStep
-        currentCol += colStep
     }
 
-    return true
+    return result
 }
 
 function isValidQueenMove(source, target, board) {
@@ -174,4 +209,101 @@ export function isValidMove(source, target, board, turn, lastMove) {
         default:
             return false
     }
+}
+
+
+//-----------------------------------------------------------------------------------------------------------------------------//
+
+
+function validPawnMoves(source, board, lastMove) {
+}
+
+function validRookMoves(source, board) {
+    let result = [];
+
+    // up
+    if (source.row > 0) {
+        result = [...result, ...validateRange(source, { row: 0, col: source.col }, board, -1, 0)]
+    }
+    // down
+    if (source.row < 7) {
+        result = [...result, ...validateRange(source, { row: 7, col: source.col }, board, 1, 0)]
+    }
+    // left
+    if (source.col > 0) {
+        result = [...result, ...validateRange(source, { row: source.row, col: 0 }, board, 0, -1)]
+    }
+    // right
+    if (source.col < 7) {
+        result = [...result, ...validateRange(source, { row: source.row, col: 7 }, board, 0, 1)]
+    }
+
+    console.log("result: ", result)
+    return result
+}
+
+function validKnightMoves(source, board) {
+}
+
+function validBishopMoves(source, board) {
+}
+
+function validQueenMoves(source, board) {
+}
+
+function validKingMoves(source, board) {
+}
+
+
+export function validMoves(source, board, turn, lastMove) {
+    let result = [];
+    // Out of bounds
+    if (!(0 <= source.row <= 7) ||
+        !(0 <= source.col <= 7)) {
+        return result
+    }
+
+    const sourcePiece = board[source.row][source.col]
+
+    // No piece to move
+    if (!sourcePiece) {
+        return result
+    }
+
+    // Not player's turn
+    if (sourcePiece.color !== turn) {
+        return result
+    }
+
+    switch (sourcePiece.type) {
+        case '♙':
+        case '♟':
+            result = validPawnMoves(source, board, lastMove)
+
+        case '♖':
+        case '♜':
+            console.log('valid rook moves')
+            result = validRookMoves(source, board)
+
+        case '♘':
+        case '♞':
+            result = validKnightMoves(source, board)
+
+        case '♗':
+        case '♝':
+            result = validBishopMoves(source, board)
+
+        case '♕':
+        case '♛':
+            result = validQueenMoves(source, board)
+
+        case '♔':
+        case '♚':
+            result = validKingMoves(source, board)
+
+        default:
+            break;
+    }
+
+    return result
 }
