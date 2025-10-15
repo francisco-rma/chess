746af3f6459db4a086b8d41ca6be9a0c0225fad8
Francisco Andrade <frma059@gmail.com>
2025-10-15 00:05:01 -0300

    valid pawn moves

--- src/ChessLogic.jsx
+++ src/ChessLogic.jsx
@@ -220,6 +220,46 @@ export function isValidMove(source, target, board, turn, lastMove) {
 
 
 function validPawnMoves(source, board, lastMove) {
+    const sourcePiece = board[source.row][source.col]
+    const direction = sourcePiece.color === 'white' ? 1 : -1
+
+    const result = []
+
+    const target = board[source.row + direction][source.col]
+    if (!target) {
+
+        result.push({ row: source.row + direction, col: source.col })
+
+        const initialRow = sourcePiece.color === 'white' ? 1 : 6
+        if (source.row === initialRow) {
+            result.push({ row: source.row + 2 * direction, col: source.col })
+        }
+
+        const lastPiece = board[lastMove.target.row][lastMove.target.col]
+        if ((lastPiece.type == '♙' || lastPiece.type == '♟')
+            && lastMove.source.col == lastMove.target.col
+            && Math.abs(lastMove.source.row - source.row) === 2
+            && Math.abs(lastMove.target.row - source.row) === 0
+            && Math.abs(lastMove.source.col - source.col) === 1
+        ) {
+            result.push({ row: source.row + direction, col: lastMove.source.col })
+        }
+    }
+
+    const leftTarget = board[source.row + direction][source.col - 1]
+    if (leftTarget && leftTarget.color !== sourcePiece.color) {
+        result.push({ row: source.row + direction, col: source.col - 1 })
+    }
+
+    const rightTarget = board[source.row + direction][source.col + 1]
+    if (rightTarget && rightTarget.color !== sourcePiece.color) {
+        result.push({ row: source.row + direction, col: source.col + 1 })
+    }
+
+
+    console.log('result: ', result)
+
+    return result
 }
 
 function validRookMoves(source, board) {
